39ba9f038d410643cc2eb194857bfea7c61115e1
Suraj Patra <surajpatra2908@gmail.com>
2023-11-04 11:22:50 +0530

    commited

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,11 +1,11 @@
 const mongoose=require("mongoose");
 
-const mongoUrl = 'mongodb://127.0.0.1:27017/Restaurant';
+// const mongoUrl = 'mongodb://127.0.0.1:27017/Restaurant';
 
 
 async function connectToDatabase() {
     try {
-      await mongoose.connect(mongoUrl, {
+      await mongoose.connect(process.env.MONGO_URI, {
         useUnifiedTopology: true
       });
       console.log("Connected to the MongoDB database successfully");
